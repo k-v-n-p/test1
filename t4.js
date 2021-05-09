f8b7a0d4627659872c9ea95814670b68d5691e00
Pranav K V N <kvnp179@gmail.com>
2021-05-10 00:09:40 +0530

    Update t4.js

--- t4.js
+++ t4.js
@@ -1,19 +1,24 @@
-1.	function createNewCertificate(
-2.	 string memory partner1, string memory partner2, address _address, string memory location
-3.	   ) public payable {
-4.	   // certificate fee must be paid
-5.	 require(msg.value >= certificateFee, "Insufficient fee");
-6.	 // new certificate creation
-7.	 MarriageCertificate newCertificate = new MarriageCertificate(
-8.	     msg.sender,
-9.	     partner1,
-10.	     partner2,
-11.	     _address,
-12.	       location);
-13.	    // we save the address in array
-14.	    certificates.push(newCertificate);
-15.	    // we update last marriage array
-16.	    lastMarriage = [partner1, partner2, location];
-17.	    // we return an event for the web3 interface
-18.	    emit LogNewCertificateCreated(newCertificate, certificates.length);
-19.	}
+function createNewCertificate(
+  string memory partner1, string memory partner2, address _address, string memory location
+  ) public payable {
+    
+   // certificate fee must be paid
+  require(msg.value >= certificateFee, "Insufficient fee");
+    
+  // new certificate creation
+   MarriageCertificate newCertificate = new MarriageCertificate(
+	     msg.sender,
+	     partner1,
+	     partner2,
+	     _address,
+	     location);
+    
+   // we save the address in array
+	    certificates.push(newCertificate);
+    
+	 // we update last marriage array
+	    lastMarriage = [partner1, partner2, location];
+    
+	 // we return an event for the web3 interface
+	    emit LogNewCertificateCreated(newCertificate, certificates.length);
+}
