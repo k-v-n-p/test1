b956129b1f4bbf9d5dfd86dfb061edf70984aa98
Pranav K V N <kvnp179@gmail.com>
2021-05-10 00:06:15 +0530

    Update t3.js

--- t3.js
+++ t3.js
@@ -1,15 +1,15 @@
-1.	function signContract() external onlySpouse {
-2.	require(isSameString(writtenContractIpfsHash, ""), "Written contract ipfs hash has been proposed yet!");
-3.	require(hasSigned[msg.sender] == false, "Spouse has already signed the contract!");
+function signContract() external onlySpouse {
+  require(isSameString(writtenContractIpfsHash, ""), "Written contract ipfs hash has been proposed yet!");
+  require(hasSigned[msg.sender] == false, "Spouse has already signed the contract!");
   
-4.	// Sender signed
-5.	hasSigned[msg.sender] = true;
+  // Sender signed
+  hasSigned[msg.sender] = true;
 
-6.	emit Signed(now, msg.sender);
+    emit Signed(now, msg.sender);
 
-7.	// Check if both spouses have signed
-8.	if (hasSigned[husbandAddress] && hasSigned[wifeAddress]) {
-9.	signed = true;
-10.	emit ContractSigned(now);
-11.	}
-12.	}
+  // Check if both spouses have signed
+  if (hasSigned[husbandAddress] && hasSigned[wifeAddress]) {
+    signed = true;
+    emit ContractSigned(now);
+  }
+}
